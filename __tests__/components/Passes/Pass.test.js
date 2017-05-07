13a4eb23e074528c179da5d283457021058b015c
TKasekamp <toniskasekamp@gmail.com>
2017-05-07 19:45:03 +0300

    Can't test more with shallow rendering

--- __tests__/components/Passes/Pass.test.js
+++ __tests__/components/Passes/Pass.test.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {mount, shallow} from 'enzyme';
 import Pass from '../../../src/components/Passes/Pass';
+import PassStatus from '../../../src/components/Passes/PassStatus';
+
+import jsdom from 'jsdom'
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
+global.document = doc
+global.window = doc.defaultView
 
 describe('Pass', () => {
     it('renders', () => {
@@ -27,5 +33,20 @@ describe('Pass', () => {
         expect(shallow(<Pass key={pass.id} groundStation={pass.groundStation} maxElevation={pass.maxElevation}
                              id={pass.id} aos={pass.aos} los={pass.los}
         />).find('td').length).toBe(7);
+    });
+
+    it('has default status', () => {
+        const pass = {
+            'id': 'random-id-0',
+            'aos': new Date().getTime() - 13022000,
+            'los': new Date().getTime() + 10232000,
+            'maxElevation': 41.5,
+            'groundStation': 'Tartu'
+        };
+
+        const item = shallow(<Pass key={pass.id} groundStation={pass.groundStation} maxElevation={pass.maxElevation}
+                                   id={pass.id} aos={pass.aos} los={pass.los}
+        />);
+        expect(item.contains(<PassStatus value={''} time={0}/>)).toBe(true);
     });
 });
