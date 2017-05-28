a7a2b85e51aa1357972f8fcdef7e0366370f556f
TKasekamp <toniskasekamp@gmail.com>
2017-05-28 18:18:32 +0300

    TLE update

--- src/components/TLE/TLECard.js
+++ src/components/TLE/TLECard.js
@@ -4,26 +4,78 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const tle =
+    {
+        'ORDINAL': '1',
+        'COMMENT': 'GENERATED VIA SPACETRACK.ORG API',
+        'ORIGINATOR': 'JSPOC',
+        'NORAD_CAT_ID': '39161',
+        'OBJECT_NAME': 'ESTCUBE 1',
+        'OBJECT_TYPE': 'PAYLOAD',
+        'CLASSIFICATION_TYPE': 'U',
+        'INTLDES': '13021C',
+        'EPOCH': '2017-05-27 19:18:36',
+        'EPOCH_MICROSECONDS': '908640',
+        'MEAN_MOTION': '14.71887908',
+        'ECCENTRICITY': '0.0009173',
+        'INCLINATION': '98.0277',
+        'RA_OF_ASC_NODE': '234.0722',
+        'ARG_OF_PERICENTER': '199.3297',
+        'MEAN_ANOMALY': '160.7569',
+        'EPHEMERIS_TYPE': '0',
+        'ELEMENT_SET_NO': '999',
+        'REV_AT_EPOCH': '21775',
+        'BSTAR': '4.4346e-05',
+        'MEAN_MOTION_DOT': '2.28e-06',
+        'MEAN_MOTION_DDOT': '0',
+        'FILE': '2165239',
+        'TLE_LINE0': '0 ESTCUBE 1',
+        'TLE_LINE1': '1 39161U 13021C   17147.80459385 +.00000228 +00000-0 +44346-4 0  9996',
+        'TLE_LINE2': '2 39161 098.0277 234.0722 0009173 199.3297 160.7569 14.71887908217754',
+        'OBJECT_ID': '2013-021C',
+        'OBJECT_NUMBER': '39161',
+        'SEMIMAJOR_AXIS': '7033.184',
+        'PERIOD': '97.833',
+        'APOGEE': '661.501',
+        'PERIGEE': '648.598'
+    };
+
 const TLECard = (props) => {
     return <div className="card">
         <div className="card-header">
             <i className="fa fa-align-justify"/> TLE
         </div>
         <div className="card-block">
-            <div className="row">
-                <code>
-                    <div> {props.tleString.name}</div>
-                    <div> {props.tleString.line1}</div>
-                    <div> {props.tleString.line2}</div>
-                </code>
-            </div>
+
+            <dl className="row">
+                <dt className="col-sm-3">Raw TLE</dt>
+                <dd className="col-sm-9"><pre><code>
+                        <div> {tle.TLE_LINE0} </div>
+                        <div> {tle.TLE_LINE1}</div>
+                        <div> {tle.TLE_LINE2}</div>
+                    </code></pre>
+                </dd>
+
+                <dt className="col-sm-3">Name</dt>
+                <dd className="col-sm-9">{tle.OBJECT_NAME}</dd>
+
+                <dt className="col-sm-3">Number</dt>
+                <dd className="col-sm-9">{tle.OBJECT_NUMBER}</dd>
+                <dt className="col-sm-3">Class</dt>
+                <dd className="col-sm-9">{tle.CLASSIFICATION_TYPE}</dd>
+                <dt className="col-sm-3">ID</dt>
+                <dd className="col-sm-9">{tle.OBJECT_ID}</dd>
+                <dt className="col-sm-3">Epoch</dt>
+                <dd className="col-sm-9">{tle.EPOCH}</dd>
+
+                <div>Other data is here, just can't be bothered to write it out.</div>
+            </dl>
         </div>
     </div>
 };
 
 TLECard.propTypes = {
-    tleString: PropTypes.string.isRequired,
-
+    tle: PropTypes.object,
 };
 
 export default TLECard;
--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -10,17 +10,7 @@ import PassEventChart from '../../components/Passes/PassEventChart';
 import TLECard from '../../components/TLE/TLECard';
 
 
-const ec1 =
-    {
-        name: 'ESTCUBE 1',
-        'line2': '2 39161  98.0494 293.0813 0011107  99.1953 261.0497 14.71695995172946',
-        'line1': '1 39161U 13021C   16209.16355149  .00000281  00000-0  53050-4 0  9997'
-    }
-;
 
-const tleString = 'ISS (ZARYA)\n' +
-    '1 25544U 98067A   08264.51782528 -.00002182  00000-0 -11606-4 0  2927\n' +
-    '2 25544  51.6416 247.4627 0006703 130.5360 325.0288 15.72125391563537';
 class Dashboard extends Component {
     componentDidMount() {
         // Get only if empty
@@ -47,7 +37,7 @@ class Dashboard extends Component {
                         <PassEventChart passes={this.props.passes} fetchState={this.props.fetchState}/>
                     </div>
                     <div className="col-lg-6">
-                        <TLECard tleString={ec1}/>
+                        <TLECard/>
                     </div>
                 </div>
 
