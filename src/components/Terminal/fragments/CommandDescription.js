23c281357e1ef3fd59be15af5f958319468b834e
Tõnis Kasekamp <tonis.kasekamp@estcube.eu>
2017-06-02 11:13:50 +0300

    Minor text fix

--- src/components/Terminal/fragments/CommandDescription.js
+++ src/components/Terminal/fragments/CommandDescription.js
@@ -8,19 +8,19 @@ const CommandDescription = (props) => {
     const params = props.commandPrototype.parameters.map((p) => {
         return <div className="row" key={p.name}>
             <div className="col-2">
-                <small className="text-uppercase text-muted mr-2">name :</small>
+                <small className="text-uppercase text-muted mr-2">name</small>
                 {p.name}
             </div>
             <div className="col-2">
-                <small className="text-uppercase text-muted mr-2">type :</small>
+                <small className="text-uppercase text-muted mr-2">type</small>
                 {p.type}
             </div>
             <div className="col-2">
-                <small className="text-uppercase text-muted mr-2">default :</small>
+                <small className="text-uppercase text-muted mr-2">default</small>
                 {p.default}
             </div>
             <div className="col-6">
-                <small className="text-uppercase text-muted mr-2">description :</small>
+                <small className="text-uppercase text-muted mr-2">description</small>
                 {p.description}
             </div>
         </div>
@@ -31,15 +31,15 @@ const CommandDescription = (props) => {
     return <div >
         <div className="row">
             <div className="col-6">
-                <small className="text-uppercase text-muted mr-2">Description :</small>
+                <small className="text-uppercase text-muted mr-2">Description</small>
                 {props.commandPrototype.description}
             </div>
             <div className="col-2">
-                <small className="text-uppercase text-muted mr-2">ID :</small>
+                <small className="text-uppercase text-muted mr-2">ID</small>
                 {props.commandPrototype.id}
             </div>
             <div className="col-4">
-                <small className="text-uppercase text-muted mr-2">Subsystems :</small>
+                <small className="text-uppercase text-muted mr-2">Subsystems</small>
                 {subs}
             </div>
         </div>
